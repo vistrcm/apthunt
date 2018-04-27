71ed58cb74d725fc6d8c3c0417cdeb752c7e21dd
Stanislav Vitko <stas@vitko.me>
2018-04-27 15:20:44 +0200

    remove tests of parse_body()

--- parser/test.py
+++ parser/test.py
@@ -1,37 +1,9 @@
 import unittest
 
-from parser import parse_body
 
 class TestParser(unittest.TestCase):
 
-    def test_parse_body(self):
-        test_body = """
-        key1: value1
-        
-        key2: value2
-
-        key3: value3
-
-        """
-        expected_result = {"key1": "value1", "key2": "value2", "key3": "value3"}
-        self.assertEqual(parse_body(test_body), expected_result)
-    
-    def test_parse_body_empty(self):
-        self.assertEqual(parse_body(""), {})
-
-    def test_parse_body_extended_empty(self):
-        empty_body = """
-
-         
-          
-            
-             
-               
-                
-
-                
-                    """
-        self.assertEqual(parse_body(empty_body), {})
+    pass
 
 
 if __name__ == '__main__':
