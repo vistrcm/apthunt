6927bd1cc7875760f9cefbcc7c94226d7d1d5b35
Stanislav Vitko <stas@vitko.me>
2018-04-27 14:32:04 +0200

    workaround on false-positives on sam-test

--- parser/test-sam.sh
+++ parser/test-sam.sh
@@ -10,4 +10,4 @@ sam local invoke ApthuntParser --event testevents/post_empty_event.json | egrep 
 # test posting some body
 sam local invoke ApthuntParser --event testevents/post_event.json | egrep "errorMessage.*stackTrace" && exit 1
 
-
+echo "looks like sam tests are ok"  # need this to not exit with 1 in case of normal execution
