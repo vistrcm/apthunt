14463bb4633cff06a9562bd6c88738117120c6c3
Stanislav Vitko <stas@vitko.me>
2018-04-29 14:53:05 +0200

    tests of parse_body()

--- parser/test.py
+++ parser/test.py
@@ -1,10 +1,28 @@
 import unittest
+import random
 
+from parser import parse_body
+from json.decoder import JSONDecodeError
 
 class TestParser(unittest.TestCase):
 
-    pass
+    def test_parse_body_empty(self):
+        with self.assertRaises(JSONDecodeError):
+                    parse_body("")
 
+    def test_parse_body_simple(self):
+        self.assertEqual(parse_body("{}"), {})
+
+    def test_parse_body_some_defined_dict(self):
+        self.assertEqual(parse_body('{"c": "d", "a": "b", "1": "2", "y": 23234}'), {"a": "b", "c": "d", "y": 23234, "1": "2"})
+
+    def test_parser_body_random_dict(self):
+        td = {}
+        for _ in range(random.randint(-100, 100)):
+            k = str(random.random())
+            v = random.random()
+            td[k] = v
+        self.assertEqual(parse_body(str(td)), td)
 
 if __name__ == '__main__':
     unittest.main()
