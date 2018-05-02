418791a7664f4d326326c7b8fdd63922611335b6
Stanislav Vitko <stas@vitko.me>
2018-05-01 23:26:46 -0700

    test-sam.sh use local dynamodb

--- parser/test-sam.sh
+++ parser/test-sam.sh
@@ -4,10 +4,20 @@ set -x
 set -v 
 set -e
 
+# start dynamodb local
+docker network create lambda-local
+docker run -d -p 8000:8000 --name dynamodb cnadiminti/dynamodb-local
+
 # test posting empty body
-sam local invoke ApthuntParser --event testevents/post_empty_event.json | egrep "errorMessage.*stackTrace" && exit 1
+sam local invoke ApthuntParser --docker-network lambda-local --event testevents/post_empty_event.json | egrep "errorMessage.*stackTrace" && exit 1
 
 # test posting some body
-sam local invoke ApthuntParser --event testevents/post_event.json | egrep "errorMessage.*stackTrace" && exit 1
+sam local invoke ApthuntParser --docker-network lambda-local --event testevents/post_event.json | egrep "errorMessage.*stackTrace" && exit 1
 
 echo "looks like sam tests are ok"  # need this to not exit with 1 in case of normal execution
+
+# do some cleanup
+docker stop dynamodb
+docker rm dynamodb
+
+docker network rm lambda-local
