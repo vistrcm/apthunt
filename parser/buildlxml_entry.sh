3ccbd1900570e07679110c0b4ba61b11cdcd8563
Stanislav Vitko <stas@vitko.me>
2018-05-04 21:06:45 -0700

    script to build lxml in container

---
+++ parser/buildlxml_entry.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+LXML_TO_INSTALL=${LXML_VER:-lxml==4.2.1}
+
+yum update
+yum install -y gcc libxml2-devel libxslt-devel
+pip install --upgrade pip
+pip install ${LXML_TO_INSTALL}
+cp -r /var/lang/lib/python3.6/site-packages/lxml* /out/
