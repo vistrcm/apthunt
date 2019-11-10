78857a4d3a679a8650d598ca56aaf70c7d8f4d75
Stanislav Vitko <stas@vitko.me>
2019-11-10 14:28:27 -0800

    test flake8 warnings

--- parser/test.py
+++ parser/test.py
@@ -13,13 +13,16 @@ class TestParser(unittest.TestCase):
 
     def test_parse_body_empty(self):
         with self.assertRaises(JSONDecodeError):
-                    parse_request_body("")
+            parse_request_body("")
 
     def test_parse_body_simple(self):
         self.assertEqual(parse_request_body("{}"), {})
 
     def test_parse_body_some_defined_dict(self):
-        self.assertEqual(parse_request_body('{"c": "d", "a": "b", "1": "2", "y": 23234}'), {"a": "b", "c": "d", "y": 23234, "1": "2"})
+        self.assertEqual(parse_request_body(
+            '{"c": "d", "a": "b", "1": "2", "y": 23234}'),
+            {"a": "b", "c": "d", "y": 23234,
+             "1": "2"})
 
     def test_parser_body_random_dict(self):
         td = {}
