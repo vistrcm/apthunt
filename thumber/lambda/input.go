7d1a0db9110418a1c545300d449d7964811b9b5a
Stanislav Vitko <stas@vitko.me>
2019-11-17 18:17:16 -0800

    add imput validation methods

---
+++ thumber/lambda/input.go
@@ -0,0 +1,34 @@
+package lambda
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+)
+
+func parseInput(input string) []string {
+	// try to load as json array
+	var records []string
+	err := json.Unmarshal([]byte(input), &records)
+	if err != nil { // looks like input is not an JSON array
+		// assume it is single URL
+		records = []string{input}
+
+	}
+	return records
+}
+
+func validateURLs(records []string) error {
+	var errs []error
+	for _, record := range records {
+		_, err := url.ParseRequestURI(record)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) > 0 { // errors found
+		return fmt.Errorf("found mailformed urls: %+v", errs)
+	}
+	return nil
+}
