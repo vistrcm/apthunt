b19cb8085adc7891e9fa72f41c228339ce5cae55
Stanislav Vitko <stas@vitko.me>
2020-05-04 23:05:53 -0700

    skeleton for processor lambda

---
+++ processor/cmd/lambda/handler.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-xray-sdk-go/xray"
+	"github.com/go-pkgz/lgr"
+)
+
+//global variables. Yes it is bad in general. This is AWS lambda handler and it is a good practice to have globals
+//nolint added because of that.
+var (
+	l *lgr.Logger //nolint:gochecknoglobals
+)
+
+//init function required for AWS lambda optimization.
+func init() { //nolint:gochecknoinits
+	//config xray
+	err := xray.Configure(xray.Config{LogLevel: "info"})
+	if err != nil {
+		panic(fmt.Sprintf("cant' configure xray: %v", err))
+	}
+
+	// init Logger, allow debug and caller info, timestamp with milliseconds
+	l = lgr.New(lgr.Msec, lgr.Debug, lgr.CallerFile, lgr.CallerFunc)
+}
+
+//Handler handler function for AWS Lambda.
+func Handler(ctx context.Context, sqsEvent events.SQSEvent) error {
+	// Start a subsegment
+	ctx, seg := xray.BeginSubsegment(ctx, "processor")
+	defer seg.Close(nil)
+
+	log.Printf("handler with context: %+v", ctx)
+
+	for _, message := range sqsEvent.Records {
+		log.Printf("The message %s for event source %s = %s\n",
+			message.MessageId, message.EventSource, message.Body)
+	}
+
+	return nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
---
+++ processor/go.mod
@@ -0,0 +1,11 @@
+module github.com/vistrcm/apthunt/processor
+
+go 1.14
+
+require (
+	github.com/aws/aws-lambda-go v1.16.0
+	github.com/aws/aws-sdk-go v1.30.20
+	github.com/aws/aws-xray-sdk-go v1.0.1
+	github.com/go-pkgz/lgr v0.7.0
+	github.com/vistrcm/apthunt/thumber v0.0.0-20200504005358-0136b10f77cc
+)
