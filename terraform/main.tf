aaa1ef0c5243f5227e39f9c23c8f72601c9d01b9
Stanislav Vitko <stas@vitko.me>
2020-05-09 13:03:33 -0700

    terrform. add modules to lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,33 @@ terraform {
 provider "aws" {
   region = "us-west-1"
 }
+
+module "processor" {
+  source = "./processor"
+  tags = local.processor_tags
+}
+
+module "parser" {
+  source = "./parser"
+  tags = local.parser_tags
+}
+
+locals {
+  # Common tags to be assigned to all resources
+  common_tags = {
+    project = "apthunt"
+    Version = "1.0"
+    Owner   = "sv"
+  }
+  # add Name for readability and merge with common tags
+  processor_tags = merge(local.common_tags,
+  {
+    "Name" = "processor",
+    "function" = "processor"
+  })
+  parser_tags = merge(local.common_tags,
+  {
+    "Name" = "apthuntparser",
+    "function" = "apthuntparser"
+  })
+}
