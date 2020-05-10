76e6ef3d64dea72ec28329bf94d30db26a77099d
Stanislav Vitko <stas@vitko.me>
2020-05-09 22:46:04 -0700

    parser. play with sam

--- parser/test-sam.sh
+++ parser/test-sam.sh
@@ -9,10 +9,12 @@ docker network create lambda-local
 docker run -d -p 8000:8000 --name dynamodb cnadiminti/dynamodb-local
 
 # test posting empty body
-sam local invoke ApthuntParser --docker-network lambda-local --event testevents/post_empty_event.json | egrep "errorMessage.*stackTrace" && exit 1
+sam local invoke ApthuntParser -v out/package --docker-network lambda-local --event testevents/post_empty_event.json | grep "Could not parse body" || exit 1
 
 # test posting some body
-sam local invoke ApthuntParser --docker-network lambda-local --event testevents/post_event.json | egrep "errorMessage.*stackTrace" && exit 1
+#sam local invoke ApthuntParser -v out/package --docker-network lambda-local --event testevents/post_event.json | egrep "errorMessage.*stackTrace" && exit 1
+sam local invoke ApthuntParser -v out/package --docker-network lambda-local --event testevents/post_event.json
+echo $?
 
 echo "looks like sam tests are ok"  # need this to not exit with 1 in case of normal execution
 
