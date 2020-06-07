e7ddeee98b952abcd1d1a121ed5e6dbed5b1095c
Stanislav Vitko <stas@vitko.me>
2020-06-06 23:18:16 -0700

    processor. extend rules

--- processor/rules.go
+++ processor/rules.go
@@ -2,32 +2,47 @@ package processor
 
 const (
 	threshold = 500
-	maxPrice  = 3000
-	minPrice  = 1500
+	maxPrice  = 3500
+	minPrice  = 2000
 )
 
 var (
 	// emulate sets with map of string->bool
 	// by default map returns "zero value", false
 	skipDistrict = map[string]bool{ //nolint: gochecknoglobals
-		"oakland east":                 true,
-		"san jose downtown":            true,
-		"sunnyvale":                    true,
-		"berkeley":                     true,
-		"Emeryville":                   true,
-		"Larkfield":                    true,
-		"san jose west":                true,
-		"berkeley north / hills":       true,
-		"oakland downtown":             true,
-		"oakland piedmont / montclair": true,
-		"oakland lake merritt / grand": true,
-		"emeryville":                   true,
-		"los gatos":                    true,
-		"redwood city":                 true,
-		"santa clara":                  true,
-		"NORTH OAKLAND TEMESCAL":       true,
-		"san jose north":               true,
-		"palo alto":                    true,
+		"oakland east":                       true,
+		"sunnyvale":                          true,
+		"berkeley":                           true,
+		"Emeryville":                         true,
+		"Larkfield":                          true,
+		"berkeley north / hills":             true,
+		"oakland downtown":                   true,
+		"oakland piedmont / montclair":       true,
+		"oakland lake merritt / grand":       true,
+		"emeryville":                         true,
+		"los gatos":                          true,
+		"redwood city":                       true,
+		"santa clara":                        true,
+		"NORTH OAKLAND TEMESCAL":             true,
+		"palo alto":                          true,
+		"san jose downtown":                  true,
+		"san jose west":                      true,
+		"san jose north":                     true,
+		"san jose east":                      true,
+		"san jose south":                     true,
+		"mountain view":                      true,
+		"Berkeley":                           true,
+		"lafayette / orinda / moraga":        true,
+		"fairfield / vacaville":              true,
+		"oakland north / temescal":           true,
+		"hayward / castro valley":            true,
+		"san mateo":                          true,
+		"concord / pleasant hill / martinez": true,
+		"soquel":                             true,
+		"cupertino":                          true,
+		"los altos":                          true,
+		"healdsburg / windsor":               true,
+		"oakland hills / mills":              true,
 	}
 )
 
